f912c0b1dafaabd7e5464772a33d94931507f96b
AAji <adeajisebutu@yahoo.com>
2013-09-02 16:47:05 -0400

    Added Pins Scaffold

---
+++ app/models/image.rb
@@ -0,0 +1,9 @@
+class Image < ActiveRecord::Base
+  attr_accessible :title, :body, :image
+
+  validates :description, :presence => :true
+  validates_attachment :image, :presence => :true,
+  								:content_type => {content_type ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
+  								:size => {less_than: 10.megabytes}
+  has_attached_file :description
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130831224612) do
+ActiveRecord::Schema.define(:version => 20130901114659) do
 
   create_table "images", :force => true do |t|
     t.string   "name"
@@ -29,9 +29,13 @@ ActiveRecord::Schema.define(:version => 20130831224612) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
