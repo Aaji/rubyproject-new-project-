1c7b536692e37398add9af3e56c0da6119842b42
AAji <adeajisebutu@yahoo.com>
2013-09-04 12:48:50 -0400

    modified app/models/images

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
 
   validates :description, :presence => :true
   validates_attachment :image, :presence => :true,
-  								:content_type => {content_type ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
+  								:content_type => {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
   								:size => {less_than: 10.megabytes}
   has_attached_file :description
 end
